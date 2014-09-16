9388dc56325b8481cdfaee5657bcec652b9b5b18
wenli <a8568730@hotmail.com>
2014-09-16 17:13:33 +0800

    加入 def __str__

--- polls/models.py
+++ polls/models.py
@@ -4,8 +4,13 @@ from django.db import models
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
-	
+	#	用來顯示物件
+	def __str__(self):
+		return	self.question_text 
+
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+	def __str__(self):
+		return	self.choice_text
