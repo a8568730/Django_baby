81313c0978cb365d085eee7dee586aee40d8d0e7
wenli <a8568730@hotmail.com>
2014-09-16 15:44:25 +0800

    建立資料庫的表格模型

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+	qustion_text = models.CharField(max_length=200)
+	pub_date = models.DataTimeField('date published')
+	
+class Choice(models.Model):
+	question = models.ForeignKey(Question)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
