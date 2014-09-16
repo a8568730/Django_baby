a0b31ed184fed3a5614567b53043b0f7652fa2c0
wenli <a8568730@hotmail.com>
2014-09-16 17:03:24 +0800

    建立了一筆問題

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-	qustion_text = models.CharField(max_length=200)
+	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 	
 class Choice(models.Model):
