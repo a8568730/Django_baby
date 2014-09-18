95a240cd766e8ed8d5f24c0fabe39fe51adf8d31
wenli <a8568730@hotmail.com>
2014-09-18 11:34:11 +0800

    在問題一次出現三個選項

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 # Register your models here.
-
+# 新增問題時，一次出現三個選項
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
 # 時間欄位上方有標題
 class QuestionAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None, 	{'fields':['question_text']}),
 		('Date information', {'fields':['pub_date']})
 	]
-
+	inlines = [ChoiceInline]
 admin.site.register(Question, QuestionAdmin)	
 admin.site.register(Choice)
