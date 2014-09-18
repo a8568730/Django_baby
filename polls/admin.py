cb0a3733231c2f72e7b962a9c65aa4211b7a62f3
wenli <a8568730@hotmail.com>
2014-09-18 11:38:15 +0800

    list_display搜尋符合欄位的項目

--- polls/admin.py
+++ polls/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 # Register your models here.
-# 新增問題時，一次出現三個選項
 class ChoiceInline(admin.TabularInline):
+	# 新增問題時，一次出現三個選項
 	model = Choice
 	extra = 3
-# 時間欄位上方有標題
+
 class QuestionAdmin(admin.ModelAdmin):
+	# 時間欄位上方有標題
 	fieldsets = [
 		(None, 	{'fields':['question_text']}),
 		('Date information', {'fields':['pub_date']})
 	]
+	# 水平顯示每個選項和投票數
 	inlines = [ChoiceInline]
+	# 搜尋問題清單
+	search_fields = ['question_text']
+	# 問題清單的資訊
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
 admin.site.register(Question, QuestionAdmin)	
 admin.site.register(Choice)
