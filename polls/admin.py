ab8d9c2429205981281d6353936ed4aeba008e98
wenli <a8568730@hotmail.com>
2014-09-18 11:28:13 +0800

    時間欄位上方有標題

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from polls.models import Question
-from polls.models import Choice
+from polls.models import Question, Choice
 # Register your models here.
-admin.site.register(Question)	
+
+# 時間欄位上方有標題
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, 	{'fields':['question_text']}),
+		('Date information', {'fields':['pub_date']})
+	]
+
+admin.site.register(Question, QuestionAdmin)	
 admin.site.register(Choice)
